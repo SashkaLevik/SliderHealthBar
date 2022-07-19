f5cc410ae03f9b861753bd018636ae16f5a6587a
SashkaLevik <levik123@yandex.ru>
2022-07-20 02:41:00 +0300

    Init

---
+++ Assets/Scripts/HealthBar.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class HealthBar : MonoBehaviour
+{
+    [SerializeField] private Slider _slider;
+    [SerializeField] private Player _player;
+    [SerializeField] private float _speedHealthChange;
+
+
+    private void Start()
+    {
+        _slider.maxValue = _player.MaxHealth;
+        _slider.value = _player.MaxHealth;
+    }
+
+    private void OnEnable()
+    {
+        _player.HealthDamage += OnHealthChange;
+    }
+
+    private void OnDisable()
+    {
+        _player.HealthDamage -= OnHealthChange;
+    }
+    
+    private IEnumerator ChangingHealthSlider(float target)
+    {
+        while (_slider.value != target) 
+        {
+            _slider.value = Mathf.MoveTowards(_slider.value, target, _speedHealthChange * Time.deltaTime);
+            yield return null;
+        }        
+    }
+
+    private void OnHealthChange(float value)
+    {
+        StartCoroutine(ChangingHealthSlider(value));
+    }
+}
+
+
+
+
+
+
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float _maxHealth = 100;
+    [SerializeField] private float _minHealth = 0;
+
+    private float _currentHealth;
+
+    public event UnityAction<float> HealthDamage;
+
+    public float MaxHealth => _maxHealth;
+
+    private void Start()
+    {
+        _currentHealth = _maxHealth;
+    }
+
+    public void TakeDamage(float damage) 
+    {
+        _currentHealth -= damage;
+        _currentHealth = Mathf.Clamp(_currentHealth, _minHealth, _maxHealth);
+        HealthDamage?.Invoke(_currentHealth);
+    }
+
+    public void Heal(float heal)
+    {
+        _currentHealth += heal;
+        _currentHealth = Mathf.Clamp(_currentHealth, _minHealth, _maxHealth);
+        HealthDamage?.Invoke(_currentHealth);
+    }
+}
